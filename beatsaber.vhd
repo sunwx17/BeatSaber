3ea30e9c188515224db81054c345ad9197b71ee2
sunwx17 <35945622+sunwx17@users.noreply.github.com>
2019-05-14 08:49:18 +0800

    init

---
+++ beatsaber.vhd
@@ -0,0 +1,19 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity beatsaber is
+	generic(
+	);
+	port(
+	);
+end;
+
+architecture main of beatsaber is
+	
+begin
+	
+end;
+	
+	
---
+++ game_logic.vhd
@@ -0,0 +1,20 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity game_logic is
+	generic(
+	);
+	port(
+		
+	);
+end;
+
+architecture game_logic_bhv of game_logic is
+	
+begin
+	
+end;
+	
+	
